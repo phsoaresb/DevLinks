906b1e727b5e6f64b03b20ba9fe9378b50401f0f
Pedro Soares <phsoaresb@hotmail.com>
2024-01-05 22:18:33 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function toggleMode() {
+    const html = document.documentElement
+    html.classList.toggle("light")
+  
+    // pegar a tag img
+    const img = document.querySelector("#profile img")
+  
+
+    }
